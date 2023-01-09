968888e86004d46365469573e62602b3cb6da6e5
fabriccio27 <fabricio_inc27@hotmail.com>
2023-01-09 09:16:06 -0300

    Add docker compose file

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. (running in a container, that updates)
+          Edit <code>src/App.js</code> and save to reload. (running in a container, that updates, with compose)
         </p>
         <a
           className="App-link"
