13cea68600f379ec8737feef922ce610396c63f9
fabriccio27 <fabricio_inc27@hotmail.com>
2023-02-07 08:58:10 -0300

    Update paragraph text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. (changed in feature branch, and also in deploy file)
+          Edit <code>src/App.js</code> and save to reload. (changed in feature branch, and also in deploy file. But reverted the last one)
         </p>
         <a
           className="App-link"
